80db62ffd4ae9bf7db5f9512e00b9ed803484e37
shonurm77 <shonurm777@gmail.com>
2024-02-26 19:38:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const name = prompt('Введите свое имя:')
+
+const age = prompt('Введите ваш возраст:');
+
+const example1 = prompt('Решите пример: 6 + 3 = ?')
+const example2 = prompt('Решите пример: 20 - 5 = ?')
+const example3 = prompt('Решите пример: 15 * 2 = ?')
+const example4 = prompt('Решите пример: 8 / 2 = ?')
+
+alert('Зайдите в консоль !')
+
+console.log("Ваше имя:" + " " + name);
+console.log("Ваше возраст:" + " " + age);
+console.log("Пример 1: 5 + 4 = 9; Ваш ответ:" + " " + example1);
+console.log("Пример 2: 20 - 5 = 15; Ваш ответ:" + " " + example2);
+console.log("Пример 3: 15 * 2 = 30; Ваш ответ:" + " " + example3);
+console.log("Пример 4: 8 / 2 = 4; Ваш ответ:" + " " + example4);
+
+
